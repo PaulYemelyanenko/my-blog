a5f88d4282666f216f19e7d740e922269ae04e85
Paul <redchik091089@gmail.com>
2020-02-25 15:04:13 +0200

    STYLE:Added main image in header and styled header
    Insert resized image(726x310) and changed style in heder nav bar

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,11 +1,9 @@
 <div id="header">
     <ul>
         <li><a href="/contact">contact</a></li>
-        <li><a href="#">stuff</a></li>
-        <li><a href="#">links</a></li>
         <li><a href="/">Home</a></li>
     </ul>
-    <h1>Paul<br />
+    <h1>Paul
         Yemelianenko<br/>
-        Blog<a name="top"></a></h1>
+        <span id="span_blog">Blog</span><a name="top"></a></h1>
 </div>
